599138deade62b3d3ab74ca3c1cca77c71a1d03e
cccc <905141352@qq.com>
2022-04-19 14:08:53 +0800

    style: 删除冗余代码

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
-// 引入等比适配插件
-const px2rem = require('postcss-px2rem');
-
-// 配置基本大小
-const postcss = px2rem({
-  // 基准大小 baseSize，需要和rem.js中相同
-  remUnit: 16,
-});
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // css: {
-  //   loaderOptions: {
-  //     less: {
-  //       javascriptEnabled: true,
-  //     },
-  //     postcss: {
-  //       plugins: [
-  //         postcss,
-  //       ],
-  //     },
-  //   },
-  // },
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].title= '个人简历'
+          return args
+        })
+  }
 })
