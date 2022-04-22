2bfbed08f02bd592cc7586e701c5e9a624ed9e13
cccc <905141352@qq.com>
2022-04-22 11:34:08 +0800

    style: 调整样式

--- src/App.vue
+++ src/App.vue
@@ -36,30 +36,5 @@ export default {
 @import "assets/scss/index";
 @import "assets/typo.css";
 
-//公共部分
-body {
-  background: #e5e5e5 url(@/assets/images/background.png);
-  width: 100%;
-  overflow-x: hidden;
-  padding: 1rem 10% 0;
-  box-sizing: border-box;
-  font-size: .9em;
-
-  .info-unit {
-    margin-bottom: 1rem;
-
-    h2 {
-      font-weight: 700;
-    }
-
-    h3 {
-      font-weight: 300;
-    }
-
-    .container-icon {
-      margin-right: .3em;
-    }
-  }
-}
 
 </style>
