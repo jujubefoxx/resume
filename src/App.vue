63afe61d08b5743396c50dc43a8ab2a4478dcdde
cccc <905141352@qq.com>
2022-04-25 16:50:20 +0800

    style: 新增背景音乐按钮

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <div class="music" ref="musicBtn" @click="clickMusic">
+      <audio loop="loop" preload="auto" autoplay="autoplay" hidden id="audio"
+             :src="musicUrl">
+      </audio>
+    </div>
     <HelloWorld/>
     <!-- 侧栏 -->
     <aside>
@@ -19,14 +24,31 @@ export default {
   name: 'App',
   data() {
     return {
+      musicUrl: '/backgroundMusic.mp3',
+      music: {}, // 背景音乐
+      musicInit: false, // 是否初始化音乐
       sideList: [{
         title: '源代码',
         link: 'https://github.com/jujubefoxx/homepage'
       }]
     }
   },
+  mounted() {
+    this.$nextTick(() => {
+      this.music = document.getElementById("audio");//背景音乐对象
+    })
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    clickMusic() {
+      if (this.music.paused) {
+        this.music.play();
+      } else {
+        this.music.pause();
+      }
+    }
   }
 }
 </script>
@@ -36,5 +58,12 @@ export default {
 @import "assets/scss/index";
 @import "assets/typo.css";
 
+.music {
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  background: url(@/assets/images/music.png);
+  background-size: 30px;
+}
 
 </style>
